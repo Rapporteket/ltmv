20884674e44275fc481613f4939377c93e0dacd2
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2021-03-29 10:35:14 +0000

    Lagt til ki-funksjon for blodgassmåling
    SVN-revisjon: 6695

--- felles/pakker/ltmv/R/kvalitetsindikatorer-ltmv.R
+++ felles/pakker/ltmv/R/kvalitetsindikatorer-ltmv.R
@@ -56,3 +56,27 @@ regn_insprev = function(d, var_folk, insidens = TRUE, ...) {
     distinct(!!!var_gruppe, .keep_all = TRUE)
   d
 }
+
+#' Kvalitetsindikator for blodgass
+#'
+#' @description
+#' Lag KI-datasett for andel forløp som fikk utført blodgassmåling ved start.
+#'
+#' @param d_ventreg Basisskjema.
+#'
+#' @return KI-datasett egnet for bruk med [rapwhale::aggreger_ki_prop()].
+#'
+#' @export
+
+ki_blodgass = function(d_ventreg) {
+  d_ventreg %>%
+    mutate(
+      ki_krit_teller = (!is.na(pco2_air) |
+        !is.na(po2_air) |
+        !is.na(capillarypo2_air) |
+        !is.na(capillarypco2_air) |
+        !is.na(be) |
+        !is.na(arterialpco2_air)),
+      ki_krit_nevner = !is.na(dato_start)
+    )
+}
