95e6e5070ff303cdb1a9b2c0243949bae8f196b1
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2021-04-15 10:41:35 +0000

    Lagt til KI-funksjon for elektiv behandling
    SVN-revisjon: 6772

--- felles/pakker/ltmv/R/kvalitetsindikatorer-ltmv.R
+++ felles/pakker/ltmv/R/kvalitetsindikatorer-ltmv.R
@@ -80,3 +80,24 @@ ki_blodgass = function(d_ventreg) {
       ki_krit_nevner = !is.na(dato_start)
     )
 }
+
+#' Kvalitetsindikator for elektiv behandling
+#'
+#' @description
+#' Lag KI-datasett for andel forløp med elektiv (planlagt) behandlingsstart.
+#'
+#' @param d_ventreg Basisskjema.
+#'
+#' @return KI-datasett egnet for bruk med [rapwhale::aggreger_ki_prop()].
+#'
+#' @export
+
+ki_elektiv = function(d_ventreg) {
+  d_ventreg %>%
+    mutate(
+      ki_krit_teller = situation_ventilation == 2,
+      ki_krit_nevner = situation_ventilation == 1 |
+        situation_ventilation == 2 |
+        situation_ventilation == 9
+    )
+}
