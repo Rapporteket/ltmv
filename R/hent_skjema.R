94f2b1d127ba956d86b31736d38ccbdb58f09d6b
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2023-01-12 14:25:35 +0100

    Funksjon for å henta all data frå eitt skjema

---
+++ R/hent_skjema.R
@@ -0,0 +1,36 @@
+#' @import dplyr
+NULL
+
+#' Hent skjema
+#'
+#' @description
+#' Hentar all data frå eitt skjema.
+#'
+#' @param skjemanamn
+#' Tekststreng med namn på skjemaet som skal hentast.
+#' @param registernamn
+#' Tekststreng med namn på register. Standardverdi er "ltmv".
+#' Argumentet er med for å kunna køyra testar på testdatabase.
+#'
+#' @details
+#' Fuksjonen brukar [rapbase::loadRegData()]
+#' til å henta all data frå skjemaet med namn `skjemanamn`:
+#' `SELECT * FROM skjemanamn`
+#'
+#' I databasen er det brukt store bokstavar i variabelnamna.
+#' I utdata frå denne funksjonen er dei gjort om til små bokstavar med
+#' [stringr::str_to_lower()].
+#'
+#' @return
+#' Tibble med all data frå skjemaet med namn `skjemanamn`.
+#'
+#' @export
+#'
+#' @examples
+#' hent_skjema("ventreg")
+hent_skjema = function(skjemanamn, registernamn = "ltmv") {
+  sporring = paste0("SELECT * FROM ", skjemanamn)
+  rapbase::loadRegData(registernamn, sporring) %>%
+    tibble::as_tibble() %>%
+    rename_with(stringr::str_to_lower)
+}
