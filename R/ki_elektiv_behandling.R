845871a097b5705e54b06c120428a51441bd788b
Ørjan Løseth Skaar <orjan.loseth.skaar@helse-bergen.no>
2024-06-18 14:20:25 +0200

    Flyttet ki for elektiv behandling i egen fil

---
+++ R/ki_elektiv_behandling.R
@@ -0,0 +1,21 @@
+#' Kvalitetsindikator for elektiv behandling
+#'
+#' @description
+#' Lag KI-datasett for andel forløp med elektiv (planlagt) behandlingsstart.
+#'
+#' @param d_ventreg Basisskjema.
+#'
+#' @return KI-datasett egnet for bruk med [rapwhale::aggreger_ki_prop()].
+#'
+#' @export
+
+ki_elektiv = function(d_ventreg) {
+  d_ventreg %>%
+    mutate(
+      ki_krit_teller = situation_ventilation == 2,
+      ki_krit_nevner = !is.na(situation_ventilation) &
+        (situation_ventilation == 1 |
+           situation_ventilation == 2 |
+           situation_ventilation == 9)
+    )
+}
