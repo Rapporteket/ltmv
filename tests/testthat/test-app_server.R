f52b34eb3c3d0e3afab90077c06faffbef46c6fa
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2025-06-02 10:13:05 +0200

    Mellombels utkommentert testar som er avhengige av testdatabase

--- tests/testthat/test-app_server.R
+++ tests/testthat/test-app_server.R
@@ -1,44 +1,44 @@
-# store current instance and set temporary config
-current_config_path = Sys.getenv("R_RAP_CONFIG_PATH")
-
-# make pristine and dedicated config to avoid interference with other tests
-Sys.setenv(R_RAP_CONFIG_PATH = file.path(tempdir(), "autoReportTesting")) # nolint: undesirable_function_linter.
-dir.create(Sys.getenv("R_RAP_CONFIG_PATH"))
-file.copy(
-  system.file(c("rapbaseConfig.yml", "dbConfig.yml", "autoReport.yml"),
-    package = "rapbase"
-  ),
-  Sys.getenv("R_RAP_CONFIG_PATH")
-)
-
-registry_name = "rapbase"
-
-
-# rapbase modules are already tested. For now, just make sure the server runs
-# by dummy class test of auto report list
-test_that("server can run", {
-  shiny::testServer(app = app_server, {
-    expect_type(report, "list")
-  })
-})
-
-# Mellombels utkommentert kode, før det finst rapportar i appen
-# nolint start: commented_code_linter.
-# test_that("example report can be provided for ui", {
+# # store current instance and set temporary config
+# current_config_path = Sys.getenv("R_RAP_CONFIG_PATH")
+#
+# # make pristine and dedicated config to avoid interference with other tests
+# Sys.setenv(R_RAP_CONFIG_PATH = file.path(tempdir(), "autoReportTesting")) # nolint: undesirable_function_linter.
+# dir.create(Sys.getenv("R_RAP_CONFIG_PATH"))
+# file.copy(
+#   system.file(c("rapbaseConfig.yml", "dbConfig.yml", "autoReport.yml"),
+#     package = "rapbase"
+#   ),
+#   Sys.getenv("R_RAP_CONFIG_PATH")
+# )
+#
+# registry_name = "rapbase"
+#
+#
+# # rapbase modules are already tested. For now, just make sure the server runs
+# # by dummy class test of auto report list
+# test_that("server can run", {
 #   shiny::testServer(app = app_server, {
-#     expect_true("list" %in% class(output$ex_report))
+#     expect_type(report, "list")
 #   })
 # })
-
-# test_that("example report can be provided for download", {
-#   shiny::testServer(app = app_server, {
-#     session$setInputs(format_report = "html")
-#     session$setInputs(download_report = 1)
-#     expect_true(file.exists(output$download_report))
-#   })
-# })
-# nolint end
-
-
-# Restore instance
-Sys.setenv(R_RAP_CONFIG_PATH = current_config_path) # nolint: undesirable_function_linter.
+#
+# # Mellombels utkommentert kode, før det finst rapportar i appen
+# # nolint start: commented_code_linter.
+# # test_that("example report can be provided for ui", {
+# #   shiny::testServer(app = app_server, {
+# #     expect_true("list" %in% class(output$ex_report))
+# #   })
+# # })
+#
+# # test_that("example report can be provided for download", {
+# #   shiny::testServer(app = app_server, {
+# #     session$setInputs(format_report = "html")
+# #     session$setInputs(download_report = 1)
+# #     expect_true(file.exists(output$download_report))
+# #   })
+# # })
+# # nolint end
+#
+#
+# # Restore instance
+# Sys.setenv(R_RAP_CONFIG_PATH = current_config_path) # nolint: undesirable_function_linter.
