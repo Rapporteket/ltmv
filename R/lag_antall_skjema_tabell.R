904adeef53b7bce2370e7eb9449ae92dfde3957b
Sergio Carracedo Huroz <sergio.carracedo.huroz@helse-bergen.no>
2025-10-23 12:32:19 +0200

    Lagt til objekt `d_centretype` i `formater_antall_skjema_tabell()`

--- R/lag_antall_skjema_tabell.R
+++ R/lag_antall_skjema_tabell.R
@@ -242,6 +242,9 @@ aggreger_antall_skjema_tabell = function(d_skjemaoversikt, user_role, resh_id) {
 #' ltmv:::formater_antall_skjema_tabell(d_antall_skjema)
 #' }
 formater_antall_skjema_tabell = function(d_antall_skjema) {
+  d_centretype = hent_skjema("centretype") |>
+    select(id, name)
+
   d_antall_skjema |>
     knitr::kable("html", col.names = NULL, format.args = list(big.mark = " ")) |>
     kableExtra::add_header_above(
