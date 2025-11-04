a9e7e1f006e57ce53b8870e05605f87a2c221a71
Ørjan Løseth Skaar <orjan.loseth.skaar@helse-bergen.no>
2025-11-04 16:05:39 +0100

    Datasett på superbredt format til bruk på Rapporteket
    Funksjon som gir ut data på superbredt format og
    viser data basert på ventreg-resh for lokale brukere.

--- R/superbreitt_format.R
+++ R/superbreitt_format.R
@@ -1,3 +1,33 @@
+lag_datasett_superbreitt_dashboard = function(fra,
+                                              til,
+                                              alderkategori,
+                                              kjonn,
+                                              inkluder_missing,
+                                              resh_id,
+                                              user_role) {
+  d_superbreitt_dashboard = superbreitt_format(
+    d_full_patientlist = hent_skjema("patient"),
+    d_full_mce = hent_skjema("mce"),
+    d_full_ventreg = hent_skjema("ventreg"),
+    d_full_ventfol = hent_skjema("ventfol"),
+    d_full_conclude = hent_skjema("conclude")
+  ) |>
+    legg_til_alder_og_kategori(p_Fodselsdato, r_start_date) |>
+    filter(
+      r_start_date >= !!fra | (is.na(r_start_date) & inkluder_missing),
+      r_start_date <= !!til | (is.na(r_start_date) & inkluder_missing),
+      alderkat %in% !!alderkategori |
+        (is.na(alderkat) & "" %in% !!alderkategori),
+      p_Kjonn %in% !!kjonn | (is.na(p_Kjonn) & "" %in% !!kjonn)
+    )
+
+  if (user_role != "SC") {
+    d_superbreitt_dashboard = filter(d_superbreitt_dashboard, r_centreid == !!resh_id) # FIXME Vert ikkje heilt rett med r_centreid her
+  }
+
+  d_superbreitt_dashboard
+}
+
 superbreitt_format = function(d_full_patientlist,
                               d_full_mce,
                               d_full_ventreg,
