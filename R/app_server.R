03245fb3d8318d62d85a7cee748ec76436962a12
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2025-11-13 14:47:22 +0100

    Nye namn på argument i exportUCServer() i rapbase v3.0.0

--- R/app_server.R
+++ R/app_server.R
@@ -207,5 +207,5 @@ app_server = function(input, output, session) {
 
   # export
   rapbase::exportGuideServer("ltmv-export", registry_name)
-  rapbase::exportUCServer("ltmv-export", registryName = "data", repoName = "ltmv")
+  rapbase::exportUCServer("ltmv-export", dbName = "data", teamName = "ltmv")
 }
